e7d9d288994f9ed4dea3990c5b1b48830579b3a1
pourquoi <mathias.dusautoy@gmail.com>
2020-08-27 18:42:01 +0200

    fix simple example

--- arduino/examples/sensor_monitoring/sensor_monitoring.ino
+++ arduino/examples/sensor_monitoring/sensor_monitoring.ino
@@ -5,7 +5,7 @@
 
 char ssid[] = "my_wifi_ssid";
 char pass[] = "my_wifi_password";
-char device[] = "my_device";
+char device[] = "my_device_name";
 
 WiFiClient wifi;
 HttpClient client = HttpClient(wifi, "cataclop.fr", 9006);
@@ -14,6 +14,18 @@ PousPousApi api = PousPousApi(client, device);
 
 void setup() 
 {
+    WiFi.begin(ssid, pass);
+
+    Serial.print("Connecting to WiFi");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println();
+    Serial.print("Connected to WiFi, IP address: ");
+
+    api.createDevice();
 }
 
 void loop()
